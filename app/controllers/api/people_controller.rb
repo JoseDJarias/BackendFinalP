d6dd2891c4b64518cf6d27f1ce8ed0d74539ad24
JoseDJarias <arias_da044@hotmail.com>
2024-01-05 15:11:11 -0600

    people actions as show update

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,4 +1,6 @@
 class Api::PeopleController < ApplicationController
+    before_action :authorization
+
     def show
 
         person = Person.find(params[:id])   
@@ -6,20 +8,20 @@ class Api::PeopleController < ApplicationController
         render json: person
     end
 
-  def update
-    @person = Person.find(params[:id])
+    def update
+        @person = Person.find(params[:id])
 
-    if @person.update(person_params)
-      render json: { message: 'A profile was successfully updated.' }, status: :ok
-    else
-      render json: { errors: @person.errors.full_messages }, status: :unprocessable_entity
+        if @person.update(person_params)
+        render json: { message: 'A profile was successfully updated.' }, status: :ok
+        else
+        render json: { errors: @person.errors.full_messages }, status: :unprocessable_entity
+        end
     end
-  end
 
-  private
+    private
 
-  def person_params
-    params.require(:person).permit(:name, :email, :password) # Adjust attributes based on your person model
-  end
+    def person_params
+        params.require(:person).permit(:name, :email, :password) # Adjust attributes based on your person model
+    end
 
 end
